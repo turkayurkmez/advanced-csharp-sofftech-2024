2e1adc9a7cbf046a5726f41dce99039feb6536c8
türkay ürkmez <turkay.urkmez@outlook.com>
2024-11-12 12:31:50 +0300

    Anoymous type and LINQ

---
+++ LINQEssentials/LINQConcept/AnonymousType/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousType
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class ProductService
+    {
+        public List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new(){ Id=1, Name="P1", Description="P1 Açıklama", Price=5, Color="Black"},
+                new(){ Id=2, Name="P2", Description="P2 Açıklama", Price=15, Color="Orange"},
+                new(){ Id=3, Name="P3", Description="P3 Açıklama", Price=25, Color="Red"},
+                new(){ Id=4, Name="P4", Description="P4 Açıklama", Price=35, Color="Pink"},
+
+            };
+        }
+    }
+
+    public class SpecialProductInfo
+    {
+        public string UrunAdi { get; set; }
+        public string Renk { get; set; }
+    }
+}
+
---
+++ LINQEssentials/LINQConcept/AnonymousType/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AnonymousType;
+
+IEnumerable<string> getStrings()
+{
+    return new List<string> { "A", "B", "C" };
+}
+Console.WriteLine("Hello, World!");
+
+var anonym = new { Name = "Türkay", IsOnline = true };
+var letters = getStrings();
+
+var products = new ProductService().GetProducts();
+var query = from product in products
+            where product.Price > 10
+            orderby product.Name
+            select new SpecialProductInfo { UrunAdi = product.Name, Renk = product.Color };
+
+//   select * from products
+foreach (var item in query)
+{
+    Console.WriteLine($"{item.UrunAdi}, {item.Renk}");
+}
+//ya LINQ olmasaydı?
+List<SpecialProductInfo> list = new List<SpecialProductInfo>();
+foreach (var item in products)
+{
+    if (item.Price > 10)
+    {
+        list.Add(new SpecialProductInfo { Renk = item.Color, UrunAdi = item.Name });
+    }
+
+}
+//Extension metod ile LINQ:
+var queryWithExtension = products.Where(p => p.Price > 10)
+                                 .OrderBy(p=>p.Name)
+                                 .ToList();
+int x = 5;
+
